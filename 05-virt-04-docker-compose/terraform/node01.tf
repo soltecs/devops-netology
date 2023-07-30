1eb4055649738b18cc0721c203cf27728a907efc
Yuri Rykov <604@vea.dev>
2023-07-30 19:11:13 +0300

    Оркестрация группой Docker-контейнеров на примере Docker Compose

---
+++ 05-virt-04-docker-compose/terraform/node01.tf
@@ -0,0 +1,30 @@
+resource "yandex_compute_instance" "node01" {
+  name                      = "node01"
+  zone                      = "ru-central1-a"
+  hostname                  = "node01.netology.cloud"
+  allow_stopping_for_update = true
+
+  resources {
+    cores  = 8
+    memory = 8
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id    = "${var.debian-11-base}"
+      name        = "root-node01"
+      type        = "network-nvme"
+      size        = "50"
+    }
+  }
+
+  network_interface {
+    subnet_id = "${yandex_vpc_subnet.default.id}"
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "yurykov:${file("~/.ssh/id_yc.pub")}"
+    user-data = "#cloud-config\nusers:\n  - name: yurykov\n    groups: sudo\n    shell: /bin/bash\n    sudo: ['ALL=(ALL) NOPASSWD:ALL']\n    ssh-authorized-keys:\n      - ${file("~/.ssh/id_yc.pub")}"
+  }
+}
