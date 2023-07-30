da0c9b696c5ca6576c92d17dce11db277fd93d97
Yuri Rykov <604@vea.dev>
2023-07-30 19:42:46 +0300

    Оркестрация группой Docker-контейнеров на примере Docker Compose

--- 05-virt-04-docker-compose/terraform/provider.tf
+++ 05-virt-04-docker-compose/terraform/provider.tf
@@ -9,7 +9,7 @@ terraform {
 
 provider "yandex" {
   # service_account_key_file = "key.json"
-  token = ''
+  token = "***"
   cloud_id  = "${var.yandex_cloud_id}"
   folder_id = "${var.yandex_folder_id}"
 }
