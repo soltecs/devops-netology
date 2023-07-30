a99201dc161a706e1f72e702aa089e563ba7e3be
Yuri Rykov <604@vea.dev>
2023-07-30 21:46:34 +0300

    Оркестрация кластером Docker контейнеров на примере Docker Swarm

--- 05-virt-05-docker-swarm/src/terraform/provider.tf
+++ 05-virt-05-docker-swarm/src/terraform/provider.tf
@@ -9,7 +9,7 @@ terraform {
 
 provider "yandex" {
   #service_account_key_file = "key.json"
-  token = "y0_AgAAAABvtmFpAATuwQAAAADo-A3FlZfgT19jR2yx9UCWqT6SfWuVlh8"
+  token = "***"
   cloud_id  = "${var.yandex_cloud_id}"
   folder_id = "${var.yandex_folder_id}"
 }
